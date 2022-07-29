e8c30af70289467c98a1009566f2e471691636e7
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-29 14:44:48 -0400

    Create my app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div className="grid grid-cols-12 gap-6 my-14 lg:px-32">
+    <div className="col-span-12 p-4 text-center bg-white lg:col-span-3 rounded-2xl">Sidebar</div>
+    <div className="col-span-12 bg-white lg:col-span-9 rounded-2xl">
+      <Component {...pageProps} />
+    </div>
+  </div>
+  )
 }
 
 export default MyApp
