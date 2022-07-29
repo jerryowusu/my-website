dc5bd2b7b8ffe9fdd2cf5cfcf1ab2bb370827f2a
Jerry Owusu <jerryowusu6133@gmail.com>
2022-07-29 17:22:55 -0400

    Update background color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
       </Head>
-      <body>
+      <body className='bg-gradient-to-r from-green-300 to-blue-300'>
         <Main />
         <NextScript />
       </body>
