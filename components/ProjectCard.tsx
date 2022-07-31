2aaf7a553a3f6f7a937f43944cdc7a042e0f900d
jerryowusu <jerryowusu6133@gmail.com>
2022-07-31 21:57:25 +0000

    Add styling to the project pop-up

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -25,26 +25,43 @@ const ProjectCard: FunctionComponent<{
         <p className="my-2 text-center">{name}</p>
 
         {showDetails && (
-        <div className="grid md:grid-cols-2 absolute top-0 left-0 z-10 w-full h-auto text-black bg-gray-100">
+        <div className="grid md:grid-cols-2 absolute top-0 
+        left-0 z-10 w-full h-auto text-black bg-gray-100 p-2">
             <div>
                 <img src={image_url} alt={name} />
-                <div>
-                    <a href={github_link}>
+                <div className='flex justify-center my-4 space-x-3'>
+                    <a 
+                    href={github_link}
+                    className='flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200'
+                    >
                         <AiFillGithub /> <span>Github</span>
                     </a>
-                    <a href={deployed_url}>
+                    <a 
+                    href={deployed_url}
+                    className='flex items-center px-4 py-2 space-x-3 text-lg bg-gray-200 dark:bg-dark-200'
+                    >
                         <AiFillProject /> <span>Project</span>
                     </a>
                 </div>
             </div>
         <div>
-            {
-                tech_stacks.map((tech) => (
-                    <div key={tech}>{tech}</div>
-                ))
-            }
+            <h2 className='mb-3 text-xl font-medium md:text-2xl'>{name}</h2>
+            <h3 className='mb-3 font-medium'>{description}</h3>
+            <div className='flex flex-wrap mt-5 space-x-2 text-sm tracking-wider'>
+                {
+                    tech_stacks.map((tech) => (
+                        <span key={tech} 
+                         className='px-2 py-1 my-1 bg-gray-200 dark:bg-dark-200 rounded-sm'
+                        >
+                            {tech}
+                        </span>
+                    ))
+                }
+            </div>
         </div>
-        <button onClick={() => setShowDetails(false)}>
+        <button onClick={() => setShowDetails(false)}
+        className='absolute p-1 bg-gray-200 rounded-full top-3 right-3 focus:outline-none dark:bg-dark-200'
+        >
             <MdClose size={30} />
         </button>
         </div>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -4,11 +4,11 @@ import { projects } from '../data'
 
 const Projects = () => {
   return (
-    <div className='p-4' >
+    <div className='px-5 py-2' style={{ height: '65vh' }} >
       <nav>Navbar</nav>
       <div className='grid grid-cols-12 gap-4 my-3 relative'>
         {projects.map((project) => (
-            <div className='col-span-12 sm:col-span-6 lg:col-span-4 p-2' key={project.name}>
+            <div className='col-span-12 sm:col-span-6 lg:col-span-4 p-2 rounded-lg bg-gray-200 dark:bg-dark-200' key={project.name}>
               <ProjectCard project={project} key={project.name} />
             </div>
           ))}
