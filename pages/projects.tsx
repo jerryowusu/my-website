08595bbb98377a85381d31afb9888677a14d1a3f
jerryowusu <jerryowusu6133@gmail.com>
2022-07-31 22:57:11 +0000

    create the projects navbar and filter the categories

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectCard from '../components/ProjectCard'
-import { projects } from '../data'
+import ProjectsNavbar from '../components/ProjectsNavbar'
+import { projects as projectsData} from '../data'
+import { Category } from '../type'
 
 const Projects = () => {
+
+  const [projects, setProjects] = useState(projectsData);
+  const [active, setActive] = useState('All')
+
+  const handleFilteredCategory = (category: Category | 'All') => {
+    if(category === 'All') {
+      setProjects(projectsData);
+      setActive(category);
+      return;
+    }
+
+    const filteredCategory = projectsData.filter((project) => project.category.includes(category));
+    setProjects(filteredCategory);
+    setActive(category);
+  }
+
   return (
     <div className='px-5 py-2' style={{ height: '65vh' }} >
-      <nav>Navbar</nav>
+      <ProjectsNavbar 
+      handleFilteredCategory={handleFilteredCategory} 
+      active={active}
+      />
       <div className='grid grid-cols-12 gap-4 my-3 relative'>
         {projects.map((project) => (
             <div className='col-span-12 sm:col-span-6 lg:col-span-4 p-2 rounded-lg bg-gray-200 dark:bg-dark-200' key={project.name}>
